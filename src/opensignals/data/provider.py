c50e25091868762b94eb8dee23ffa13cb73df5fe
RobWishart <rob_wishart@hotmail.com>
2024-04-09 18:26:12 +0100

    Update provider.py

--- src/opensignals/data/provider.py
+++ src/opensignals/data/provider.py
@@ -165,6 +165,7 @@ class Provider(ABC):
                  target: str = 'target_20d',
                  feature_prefix: Optional[str] = None) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, List[str]]:
         """generate data set"""
+        print('GET DATA XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
         if last_friday is None:
             last_friday = dt.datetime.today() - relativedelta(weekday=FR(-1))
         if features_generators is None:
